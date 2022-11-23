266bedc0d7a422d958d758e40095c7f3101c0e96
Guilherme Faxina <gamerstarbr7@hotmail.com>
2022-11-23 16:47:08 -0300

    feat: caching with redis

--- server.ts
+++ server.ts
@@ -1,2 +1,31 @@
-import express from 'express';
+import express, { application } from "express";
+import axios from "axios";
+import { createClient } from "redis";
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+const client = createClient();
+
+client.connect();
+
+app.get("/", (_, res) => {
+  res.status(200).send("Hello");
+});
+
+app.get("/posts", async (_, res) => {
+  try {
+    const productsCached = await client.get("products");
+    if (!productsCached) {
+      const { data } = await axios.get("https://dummyjson.com/products/1");
+      client.setEx("products", 60, JSON.stringify(data));
+      return res.json(data);
+    }
+    res.json(JSON.parse(productsCached));
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
